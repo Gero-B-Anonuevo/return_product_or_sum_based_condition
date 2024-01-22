4245c3d7eab4666530c930e528a1a3149a9ae69a
Anonuevo <geroanonuevo3@gmail.com>
2024-01-22 10:45:44 +0800

    4th commit to try to update code, commit message updates but the code does not. Previous attemps does not work as git bash does not recognize change in file eventhough lines are deleted

--- prog.py
+++ prog.py
@@ -1,24 +1,7 @@
-# Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
-
-
-
 first_number = int(input("Input the first number: "))
 second_number = int(input("Input your second number: "))
 
 if (first_number*second_number) <= 1000:
     print(first_number*second_number)
 else:
-    print( int(first_number) + int(second_number)) 
-
-#def multiplication_or_sum(num1, num2):
-    # calculate product of two number
-#    product = num1 * num2
-    # check if product is less then 1000
-#    if product <= 1000:
-#        return product
-#   else:
-        # product is greater than 1000 calculate sum
-#        return num1 + num2
-
-#esult = multiplication_or_sum(first_number , second_number)
-#rint("The result is", result)
+    print( int(first_number) + int(second_number))
