aa4c481dbe72ba646a7e65582ab48df27fb40663
Anonuevo <geroanonuevo3@gmail.com>
2024-01-22 10:17:11 +0800

    recommended solution working

--- prog.py
+++ prog.py
@@ -1,16 +1,14 @@
 # Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
 
-#pseudocode
-#ask input
+
+
 first_number = int(input("Input the first number: "))
 second_number = int(input("Input your second number: "))
-#get product
+
 #if (first_number*second_number) <= 1000:
 #    print(first_number*second_number)
 #else:
 #    print( int(first_number) + int(second_number)) 
-#if product=<1000
-#else return sum
 
 def multiplication_or_sum(num1, num2):
     # calculate product of two number
